7065ecea49894d62b603861b52d23f4cdc4299f3
erfg12 <jake@newagesoldier.com>
2015-08-27 06:47:43 -0400

    removed memory.dll

--- cleanup.bat
+++ cleanup.bat
@@ -1,11 +1,9 @@
 for /d /r . %%d in (obj,debug) do @if exist "%%d" rd /s/q "%%d"
 del /S /F /AH *.suo
 rmdir /s /q "AutoBot\bin"
-rmdir /s /q "Memory\bin"
 rmdir /s /q "Installer\Release"
 rmdir /s /q "injectdll2\injectdll2\Release"
 cd "EQTrainer\bin\Release"
-del /Q Memory.dll
 del /S /F *.exe
 del /S /F *.pdb
 del /S /F *.config
