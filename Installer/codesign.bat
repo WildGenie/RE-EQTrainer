af54ad4e63b25fb2f74343f4ccf90219bc155a0c
Jacob Fliss <jake@newagesoldier.com>
2016-09-29 10:52:34 -0400

    changed version to 5.9.5

--- Installer/codesign.bat
+++ Installer/codesign.bat
@@ -1,5 +1,5 @@
 @echo off
 set /p id="Enter private key file pass: "
-"C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool" sign /t http://timestamp.verisign.com/scripts/timestamp.dll /f "C:\Users\jake.CUTTING\Documents\My Private Key.pfx" /p %id% "Debug\setup.exe"
-"C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool" sign /t http://timestamp.verisign.com/scripts/timestamp.dll /f "C:\Users\jake.CUTTING\Documents\My Private Key.pfx" /p %id% "Debug\Installer.msi"
+"C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool" sign /t http://timestamp.verisign.com/scripts/timestamp.dll /f "%UserProfile%\Documents\My Private Key.pfx" /p %id% "Debug\setup.exe"
+"C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool" sign /t http://timestamp.verisign.com/scripts/timestamp.dll /f "%UserProfile%\Documents\My Private Key.pfx" /p %id% "Debug\Installer.msi"
 pause
